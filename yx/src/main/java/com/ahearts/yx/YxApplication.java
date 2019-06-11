0d590970e44f5523d862dd56e89d221c3500bf9f
iTenis <xiehuisheng@hotmail.com>
2019-06-11 12:02:22 +0800

    20190611 project init

---
+++ yx/src/main/java/com/ahearts/yx/YxApplication.java
@@ -0,0 +1,13 @@
+package com.ahearts.yx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YxApplication.class, args);
+	}
+
+}
---
+++ yx/src/test/java/com/ahearts/yx/YxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ahearts.yx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
